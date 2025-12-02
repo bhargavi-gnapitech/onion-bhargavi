fcefcdcb1233142263db5f1bf6620e79d2a9eb35
bhargavi-gnapitech <bhargavi.tallapaneni@gnapitech.com>
2025-12-02 12:36:00 +0530

    Uncomment login steps and improve logging

--- src/test/gigapower/steps/gplogin.js
+++ src/test/gigapower/steps/gplogin.js
@@ -21,10 +21,10 @@ When(
 	{ timeout: 120000 },
 	 async function () {
 	// 	await login.login(username, password);
-	// }
+	
 	 await page.fill('input#login-user', 'sunaina');
      await page.fill('input#login-pass', 'iqgeo');
-	
+	 }
 );
 
 Then('User Click on Login', { timeout: 70000 }, async function () {
@@ -47,6 +47,7 @@ Then(
 
 	// 	//Check if the correct URL is loaded
 	// 	await expect(global.page).toHaveURL('https://uat.neon.iqgeo.cloud/mywcm_manager.html');
-	// }
-	console.log("Test case passed");
+		 console.log("Test case passed");
+}
+	
 );
